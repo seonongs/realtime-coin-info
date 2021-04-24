3513f989974def755d1571f097d3be7b334339f9
pronongs <pronongs@gmail.com>
2021-04-24 23:37:53 +0900

    Initial commit

---
+++ src/main/java/com/hfac/realtimecoininfo/RealtimeCoinInfoApplication.java
@@ -0,0 +1,13 @@
+package com.hfac.realtimecoininfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeCoinInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeCoinInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hfac/realtimecoininfo/RealtimeCoinInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hfac.realtimecoininfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeCoinInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
